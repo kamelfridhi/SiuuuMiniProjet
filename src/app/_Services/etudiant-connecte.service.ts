042b989fcba969ec5b55c6d6c17b912e6ef5bb14
medmehdihanini <80129416+medmehdihanini@users.noreply.github.com>
2023-12-03 00:48:02 +0100

    encript local storage value

--- src/app/_Services/etudiant-connecte.service.ts
+++ src/app/_Services/etudiant-connecte.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import  *  as CryptoJS from  'crypto-js';
 @Injectable({
   providedIn: 'root'
 })
 export class EtudiantConnecteService {
    roleas!: string ;
-
+  key = "123";
   constructor() {}
 
   public saveData(key: string, value: string) {
-    localStorage.setItem(key, value);
+    localStorage.setItem(key, this.encrypt(value));
   }
 
   public getData(key: string) {
-    return localStorage.getItem(key)
+    let data = localStorage.getItem(key)|| "";
+    return this.decrypt(data);
   }
   public removeData(key: string) {
     localStorage.removeItem(key);
@@ -23,15 +24,21 @@ export class EtudiantConnecteService {
     localStorage.clear();
   }
 is_loged():Boolean{
-    return !!localStorage.getItem('id');
+    return !!this.getData('id');
 }
 getrole(role:string):Boolean{
-  let roleas = localStorage.getItem('role');
+  let roleas = this.getData('role');
     if(roleas == role){
       return true;
     }
     return false;
 }
 
+  private encrypt(txt: string): string {
+    return CryptoJS.AES.encrypt(txt, this.key).toString();
+  }
 
+  private decrypt(txtToDecrypt: string) {
+    return CryptoJS.AES.decrypt(txtToDecrypt, this.key).toString(CryptoJS.enc.Utf8);
+  }
 }
